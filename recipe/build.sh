7100bd121bbb698cf1668e6152f01b4d361df595
Larry Shaffer <lshaffer@boundlessgeo.com>
2018-09-25 14:19:47 -0600

    Test for uname; fix bison/m4 error

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 # Avoid Xcode
-if [[ ${HOST} =~ .*darwin.* ]]; then
+if [[ $(uname) == Darwin ]]; then
   PATH=${PREFIX}/bin/xc-avoidance:${PATH}
 fi
+
+# Fix bison/m4 error
+# https://github.com/conda-forge/bison-feedstock/issues/7
+export M4=${BUILD_PREFIX}/bin/m4
 
 qmake
 make -j${CPU_COUNT}
