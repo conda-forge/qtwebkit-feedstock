06c31673bef5ff03b4659ddbea01e8ccad56fc4c
Sam Gillingham <gillingham.sam@gmail.com>
2020-08-14 12:22:16 +1000

    try g++ workaround stolen from qtlocation-feedstock

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,6 +9,13 @@ fi
 # https://github.com/conda-forge/bison-feedstock/issues/7
 export M4=${BUILD_PREFIX}/bin/m4
 
+if [[ ${HOST} =~ .*linux.* ]]; then
+  # Missing g++ workaround.
+  ln -s ${GXX} g++ || true
+  chmod +x g++
+  export PATH=${PWD}:${PATH}
+fi
+
 qmake
 make -j${CPU_COUNT}
 make install
