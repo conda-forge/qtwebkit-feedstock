6e5f6c4867f85e7b78bc8f15fde561ad3ea9c73c
Sam Gillingham <gillingham.sam@gmail.com>
2020-11-21 17:39:06 +1000

    Initial work on getting recipe working again

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,12 +1,18 @@
-#!/bin/bash
 
 mkdir build
+if errorlevel 1 exit 1
 cd build
+if errorlevel 1 exit 1
+
 cmake -G Ninja -D PORT=Qt -D CMAKE_BUILD_TYPE=Release \
     -D USE_LIBHYPHEN=OFF \
     -D CMAKE_INSTALL_PREFIX="${PREFIX}" \
     -D CMAKE_PREFIX_PATH="${PREFIX}" \
     ..
+if errorlevel 1 exit 1
 
-ninja -j $CPU_COUNT
+ninja -j %CPU_COUNT%
+if errorlevel 1 exit 1
+
 ninja install
+if errorlevel 1 exit 1
