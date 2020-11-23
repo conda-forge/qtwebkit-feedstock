3e88d0af84aaaca08a3c10adcbc0b01f5054a8f2
Sam Gillingham <gillingham.sam@gmail.com>
2020-11-23 13:21:07 +1000

    fix flag setting

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,7 @@ cmake -G Ninja -D PORT=Qt -D CMAKE_BUILD_TYPE=Release \
     -D USE_LIBHYPHEN=OFF \
     -D CMAKE_INSTALL_PREFIX="${PREFIX}" \
     -D CMAKE_PREFIX_PATH="${PREFIX}" \
-    -D MACOS_USE_SYSTEM_ICU=ON \
+    -D MACOS_USE_SYSTEM_ICU=OFF \
     ..
 
 ninja -j $CPU_COUNT
