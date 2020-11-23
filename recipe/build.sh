b2c47b936444d9990e25fbf389b38e832cf52962
Sam Gillingham <gillingham.sam@gmail.com>
2020-11-23 16:08:34 +1000

    only set OSX options on OSX

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,13 +2,19 @@
 
 mkdir build
 cd build
+
+if [ $(uname) == Darwin ]; then
+    # the default values of these require a newer SDK...
+    PLATFORM_OPTS="-D MACOS_USE_SYSTEM_ICU=OFF -D USE_UNIX_DOMAIN_SOCKETS=ON -D MACOS_FORCE_SYSTEM_XML_LIBRARIES=OFF"
+else
+    PLATFORM_OPTS=""
+fi
+
 cmake -G Ninja -D PORT=Qt -D CMAKE_BUILD_TYPE=Release \
     -D USE_LIBHYPHEN=OFF \
     -D CMAKE_INSTALL_PREFIX="${PREFIX}" \
     -D CMAKE_PREFIX_PATH="${PREFIX}" \
-    -D MACOS_USE_SYSTEM_ICU=OFF \
-    -D USE_UNIX_DOMAIN_SOCKETS=ON \
-    -D MACOS_FORCE_SYSTEM_XML_LIBRARIES=OFF \
+    $PLATFORM_OPTS \
     ..
 
 ninja -j$CPU_COUNT
