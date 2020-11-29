480844978e0afe75b1947a88008f2d661f35be44
Sam Gillingham <gillingham.sam@gmail.com>
2020-11-30 07:49:15 +1000

    Update Sources to http://download.qt.io/snapshots/ci/qtwebkit/ (#15)
    * MNT: Re-rendered with conda-build 3.20.5, conda-smithy 3.8.5, and conda-forge-pinning 2020.11.18.20.19.01
    
    * MNT: Re-rendered with conda-build 3.20.5, conda-smithy 3.8.5, and conda-forge-pinning 2020.11.20.14.18.33
    
    * MNT: Re-rendered with conda-build 3.20.5, conda-smithy 3.8.5, and conda-forge-pinning 2020.11.20.14.18.33
    
    * MNT: Re-rendered with conda-build 3.20.5, conda-smithy 3.8.5, and conda-forge-pinning 2020.11.20.14.18.33
    
    * MNT: Re-rendered with conda-build 3.20.5, conda-smithy 3.8.5, and conda-forge-pinning 2020.11.20.14.18.33
    
    * Initial work on getting recipe working again
    
    * fix line continuations
    
    * MNT: Re-rendered with conda-build 3.20.5, conda-smithy 3.8.5, and conda-forge-pinning 2020.11.21.12.44.23
    
    * fix license string and try later ruby to fix Windows
    
    * MNT: Re-rendered with conda-build 3.20.5, conda-smithy 3.8.5, and conda-forge-pinning 2020.11.21.12.44.23
    
    * try and fix build for osx and win
    
    * More patches
    
    * fix osx patch and add deps
    
    * MNT: Re-rendered with conda-build 3.20.5, conda-smithy 3.8.5, and conda-forge-pinning 2020.11.22.13.14.13
    
    * libjpeg -> jpeg
    
    * MNT: Re-rendered with conda-build 3.20.5, conda-smithy 3.8.5, and conda-forge-pinning 2020.11.22.13.14.13
    
    * try fix Windows errors and don't do QOS on OSX
    
    * use conda ICU on OSX and another go at fixing ICU linking on Windows
    
    * MNT: Re-rendered with conda-build 3.20.5, conda-smithy 3.8.5, and conda-forge-pinning 2020.11.23.01.47.34
    
    * fix flag setting
    
    * don't use OSX IPC
    
    * add libxml2
    
    * MNT: Re-rendered with conda-build 3.20.5, conda-smithy 3.8.5, and conda-forge-pinning 2020.11.23.01.47.34
    
    * only set OSX options on OSX
    
    * workaround msys2 gperf needing Unix line endings
    
    * try m2-perl on Windows and newer OSX SDK
    
    * MNT: Re-rendered with conda-build 3.20.5, conda-smithy 3.8.5, and conda-forge-pinning 2020.11.23.21.32.28
    
    * add missing file
    
    * MNT: Re-rendered with conda-build 3.20.5, conda-smithy 3.8.5, and conda-forge-pinning 2020.11.23.21.32.28
    
    * try removing some OSX patches now we are on SDK 10.12. Don't build frameowrk on OSX. Fix another newline problem on Windows
    
    * another newline patch
    
    * remove OSX patches and renumber
    
    * go back to Windows Perl to see if this fixes other error, but ensure gperf files have Unix line endings
    
    * add run_exports
    
    * add run_exports to correct section
    
    * MNT: Re-rendered with conda-build 3.20.5, conda-smithy 3.8.5, and conda-forge-pinning 2020.11.24.19.24.28
    
    * Fix Windows install paths
    
    * add a snapshot identifier to the url so we always get the same file
    
    Co-authored-by: conda-forge-linter <github-actions@email.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-qmake
-make -j $CPU_COUNT
-make install
+mkdir build
+cd build
+
+if [ $(uname) == Darwin ]; then
+    # Use Conda rather than system icu/libxml2 etc (correct?)
+    PLATFORM_OPTS="-D MACOS_USE_SYSTEM_ICU=OFF -D MACOS_BUILD_FRAMEWORKS=OFF -D MACOS_FORCE_SYSTEM_XML_LIBRARIES=OFF"
+else
+    PLATFORM_OPTS=""
+fi
+
+cmake -G Ninja -D PORT=Qt -D CMAKE_BUILD_TYPE=Release \
+    -D USE_LIBHYPHEN=OFF \
+    -D CMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -D CMAKE_PREFIX_PATH="${PREFIX}" \
+    $PLATFORM_OPTS \
+    ..
+
+ninja -j$CPU_COUNT
+ninja install
