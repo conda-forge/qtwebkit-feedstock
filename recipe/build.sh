91d603d880d325205de9d33eec944916d003c168
Sam Gillingham <gillingham.sam@gmail.com>
2023-02-14 15:20:09 +1300

    add linux_ppc64le (#27)
    * add linux_ppc64le
    
    * MNT: Re-rendered with conda-build 3.23.3, conda-smithy 3.22.1, and conda-forge-pinning 2023.02.12.11.51.20
    
    * disable JIT for ppc
    
    ---------
    
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,12 +3,17 @@
 mkdir build
 cd build
 
+echo "arch $ARCH"
 if [ $(uname) == Darwin ]; then
     # Use Conda rather than system icu/libxml2 etc (correct?)
     PLATFORM_OPTS="-D MACOS_USE_SYSTEM_ICU=OFF -D MACOS_BUILD_FRAMEWORKS=OFF -D MACOS_FORCE_SYSTEM_XML_LIBRARIES=OFF"
+elif [ "$ARCH" == "ppc64le" ]; then
+    # JIT not supported
+    PLATFORM_OPTS="-D ENABLE_JIT=OFF"
 else
     PLATFORM_OPTS=""
 fi
+echo "PLATFORM_OPTS $PLATFORM_OPTS"
 
 cmake ${CMAKE_ARGS} -G Ninja -D PORT=Qt -D CMAKE_BUILD_TYPE=Release \
     -D USE_LIBHYPHEN=OFF \
